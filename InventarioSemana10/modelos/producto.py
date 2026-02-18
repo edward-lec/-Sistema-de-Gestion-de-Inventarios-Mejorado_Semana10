ac28bc6d4165fba507abf2cbb6af2e6ea0b506ed
Leiber <le.correab@uea.edu.ec>
2026-02-18 12:44:24 -0500

    sistema de gestión de inventarios con persistencia en archivos y manejo de excepciones

---
+++ InventarioSemana10/modelos/producto.py
@@ -0,0 +1,23 @@
+
+# Clase Producto
+# Representa un producto dentro del inventario
+# creado por: Leiber Correa
+
+class Producto:
+    def __init__(self, codigo, nombre, cantidad, precio):
+        """
+        Constructor de la clase Producto.
+        Inicializa los atributos básicos del producto.
+        """
+        self.codigo = codigo
+        self.nombre = nombre
+        self.cantidad = cantidad
+        self.precio = precio
+
+    def __str__(self):
+        """
+        Método especial que convierte el objeto en texto.
+        Se usa para guardar el producto en el archivo.
+        Formato: codigo,nombre,cantidad,precio
+        """
+        return f"{self.codigo},{self.nombre},{self.cantidad},{self.precio}"
